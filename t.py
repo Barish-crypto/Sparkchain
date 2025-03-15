3fe8b286d940e613719ea0372a6536984aa1f154
barish <ngosinh737@gmail.com>
2025-03-15 14:03:00 +0700

    .

--- t.py
+++ t.py
@@ -1,9 +1,14 @@
+import ipaddress
+
 def is_valid_proxy(proxy):
     parts = proxy.split(':')
     if len(parts) != 2:
         return False
-    ip, port = parts
+    host, port = parts
     try:
+        addr = ipaddress.ip_address(host)
+        if addr.is_private or addr.is_unspecified or addr.is_loopback or addr.is_multicast or addr.is_reserved:
+            return False
         port = int(port)
         if not (0 <= port <= 65535):
             return False
